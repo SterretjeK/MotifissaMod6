3b0e4d4927d2d8107a696a876ebc0e732d670507
Frank <frankbsmn@gmail.com>
2022-01-02 12:26:06 +0100

    added the FriendsFragment and made the bottom nav usable

---
+++ app/src/main/java/com/example/motifissa/FriendsFragment.java
@@ -0,0 +1,38 @@
+package com.example.motifissa;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FriendsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FriendsFragment extends Fragment {
+
+    public FriendsFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static FriendsFragment newInstance() {
+        return new FriendsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friends, container, false);
+    }
+}
--- app/src/main/java/com/example/motifissa/MainScreen.java
+++ app/src/main/java/com/example/motifissa/MainScreen.java
@@ -1,14 +1,25 @@
 package com.example.motifissa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainScreen extends AppCompatActivity {
 
+    // fragments
     DashboardFragment dashboardFragment;
+    FriendsFragment friendsFragment;
+
+    //elements
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +35,39 @@ public class MainScreen extends AppCompatActivity {
         Bundle bundle = new Bundle();
         bundle.putString("LOGIN_NAME", loginName);
         dashboardFragment.setArguments(bundle);
-        getSupportFragmentManager().beginTransaction().replace(R.id.dashboard_container, dashboardFragment).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, dashboardFragment).commit();
 
+        // setup the friends fragment
+        friendsFragment = new FriendsFragment();
+
+        // setup the bottom navigation
+        bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.dashboard:
+                    selectedFragment = dashboardFragment;
+                    break;
+                case R.id.friends:
+                    selectedFragment = friendsFragment;
+                    break;
+//                case R.id.nav_dashboard:
+//                    selectedFragment = dashboardFragment;
+//                    break;
+            }
+            if (selectedFragment == null){
+                Toast.makeText(MainScreen.this, "SelectedFragment in navListener was null", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, selectedFragment).commit();
+            return true;
+        }
+    };
 }
