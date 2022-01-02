9bdb911e21233387ae10ad8f5db357fd0d5697aa
Frank <frankbsmn@gmail.com>
2022-01-02 14:20:04 +0100

    Added searching

--- app/src/main/java/com/example/motifissa/FriendsFragment.java
+++ app/src/main/java/com/example/motifissa/FriendsFragment.java
@@ -4,21 +4,28 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FriendsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FriendsFragment extends Fragment {
+
+    EditText searchField;
+    ListView usersList;
+    ArrayAdapter<String> listUsers_Adapter;
+    View view;
+    String[] users;
 
     public FriendsFragment() {
         // Required empty public constructor
     }
-
 
     public static FriendsFragment newInstance() {
         return new FriendsFragment();
@@ -32,7 +39,49 @@ public class FriendsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        view = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        // setup the list view for the users
+        users = new String[]{"Henkie", "Sterre", "Frank", "Jelle", "Floor", "Sil", "Henkie 2", "Sallie","Carmine","Norbert","Pam","Deon","Modesto","Isaac","Robert","Bernie","Rodrigo","Yesenia","Rosalinda","Mohammed","Britt","Candace","Ginger","Zelma","Patricia","Aurelio","Carlos","Emmitt","Garfield","Charley","Blanche","Efren","Frank","Kay","Pam","Robert","Pearlie","Imelda","Daryl","Latonya","Jami","Jere","Dwain","Randolph","Ina","Karla","Ellen","Aimee","Malcolm","Antione","Lana","Sherrie","Carlo","Anastasia","Tonya","Harris","Roslyn"};
+
+        usersList = view.findViewById(R.id.users_list);
+        listUsers_Adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, users);// sets the bluetooth devices in an adaptor
+        usersList.setAdapter(listUsers_Adapter); //add the bluetooth devices to the list view
+
+        // more complex adaptor for the list view (future improvement): https://www.tutlane.com/tutorial/android/android-listview-with-examples
+
+        usersList.setOnItemClickListener(usersListListener);
+
+        // setup the search field:
+        searchField = view.findViewById(R.id.search_friends);
+        searchField.addTextChangedListener(searchTextWatcher);
+
+        return view;
     }
+
+    private AdapterView.OnItemClickListener usersListListener = new AdapterView.OnItemClickListener(){
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(getContext(), users[position], Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private TextWatcher searchTextWatcher = new TextWatcher(){
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            listUsers_Adapter.getFilter().filter(s);
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
