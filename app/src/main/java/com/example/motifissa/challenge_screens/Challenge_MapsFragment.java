3906132cb282905fb3e6c22ce5d0c74c901ea1d6
Frank <frankbsmn@gmail.com>
2022-01-17 20:33:17 +0100

    Added the ability fot users to place a marker

--- app/src/main/java/com/example/motifissa/challenge_screens/Challenge_MapsFragment.java
+++ app/src/main/java/com/example/motifissa/challenge_screens/Challenge_MapsFragment.java
@@ -1,23 +1,43 @@
 package com.example.motifissa.challenge_screens;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.motifissa.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Objects;
+
+/**
+ * Description of how this fragment works:
+ *
+ * One user (the master) the one who started the challenge chooses a location on the map and then confirms
+ * Then the other user is shown this location and can choose to accept or to change it
+ * If accepted he chances his state and continues on then the other uses sees that state change and continues with him
+ * If he didn't accept and he can change it and sent it trough to the other user where he can do the same again
+ *
+ */
+
 public class Challenge_MapsFragment extends Fragment {
+    // initializing FusedLocationProviderClient
+    //https://techenum.com/how-to-get-current-gps-location-in-android/
+
+    Button confirmButton;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -25,16 +45,27 @@ public class Challenge_MapsFragment extends Fragment {
          * Manipulates the map once available.
          * This callback is triggered when the map is ready to be used.
          * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
          * If Google Play services is not installed on the device, the user will be prompted to
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+
+        // https://developers.google.com/maps/documentation/android-sdk/events
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            // TODO change this to the users current location:
+            LatLng userLatLng = new LatLng(52.24655176852505, 6.847529082501974);
+            googleMap.addMarker(new MarkerOptions().position(userLatLng).title("Your location"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLng));
+
+
+            Marker chosenMarker = googleMap.addMarker(new MarkerOptions().position(userLatLng).title("Place to meet").visible(false));
+
+            googleMap.setOnMapLongClickListener(latLng -> {
+                chosenMarker.setPosition(latLng);
+                chosenMarker.setVisible(true);
+                confirmButton.setEnabled(true);
+            });
         }
     };
 
@@ -43,7 +74,12 @@ public class Challenge_MapsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_challenge__maps, container, false);
+        View view = inflater.inflate(R.layout.fragment_challenge__maps, container, false);
+
+        confirmButton = view.findViewById(R.id.confirm_button);
+        confirmButton.setEnabled(false);
+
+        return view;
     }
 
     @Override
@@ -55,4 +91,6 @@ public class Challenge_MapsFragment extends Fragment {
             mapFragment.getMapAsync(callback);
         }
     }
+
+
 }
